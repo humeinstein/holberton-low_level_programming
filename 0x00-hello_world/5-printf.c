fd267ea1d6065cf01d32d82d49f0f362e1257be2
humeinstein <humeincreations@gmail.com>
2019-06-19 18:51:42 +0000

    cfile, printf command

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,5 @@
 /* add command */
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -8,6 +7,6 @@
  */
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,");
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
