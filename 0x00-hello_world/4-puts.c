2298570ef547aa0074b390b0bf5c7ea3b26c3a7e
humeinstein <humeincreations@gmail.com>
2019-06-19 18:38:35 +0000

    adding my first c file. Printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 /*puts command*/
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle.");
