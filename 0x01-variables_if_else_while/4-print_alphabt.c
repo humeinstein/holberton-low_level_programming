e65a631d5b4abdbadcb8c50afe18ec3e0ff594f6
humeinstein <humeincreations@gmail.com>
2019-06-20 21:38:08 +0000

    edit syntax line 14

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,11 +10,10 @@
 int main(void)
 {
 	char ch;
-	char ch1;
 
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
-		if (ch != q && ch != e)
+		if (ch != 'q' && ch != 'e')
 			putchar (ch);
 	}
 	putchar('\n');
