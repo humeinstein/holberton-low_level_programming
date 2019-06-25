0cbee89368231991572ad85fe5c70c11f7926469
humeinstein <humeincreations@gmail.com>
2019-06-25 23:04:04 +0000

    come back to fix error

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 	char a;
 	char counter;
 
-	for (counter = 0; counter <= 10; counter++)
+	for (counter = 0; counter <= 9; counter++)
 	{
 		for (a = 'a'; a <= 'z'; a++)
 		{
