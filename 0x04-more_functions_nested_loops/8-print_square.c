ceba92ff40ee0416cc2e539951c1c35385aae199
humeinstein <humeincreations@gmail.com>
2019-06-27 23:48:54 +0000

    add file, print square based on @size

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_square - entry
+ * @size: size of square for example size by size (4x4)
+ *
+ *
+ */
+void print_square(int size)
+{
+	int rows;
+	int col;
+
+	for (rows = 1 ; rows <= size ; rows++)
+	{
+		for (col = 1 ; col <= size ; col++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
