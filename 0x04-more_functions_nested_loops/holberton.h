6a370419a5ce0b998522af0fbefacf3782e7aeb9
humeinstein <humeincreations@gmail.com>
2019-06-27 17:50:48 +0000

    add custom header file w/ prototypes

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H_
+#define HOLBERTON_H_
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+#endif
