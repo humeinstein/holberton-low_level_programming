6e9f3df03eafc438a6e0f156447ea6f15d7746ca
humeinstein <humeincreations@gmail.com>
2019-07-01 15:24:30 -0400

    first attempt

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* reset_to_96 - entry reset with pointer
+* @*d: = address of n
+*
+*
+*/
+
+
+void reset_to_98(int *n)
+{
+	int *d = &n;
+	*d = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H_
+#define HOLBERTON_H_
+
+void reset_to_98(int *n);
+
+
+#endif // HOLBERTON_H_
+
+
+
+
+
