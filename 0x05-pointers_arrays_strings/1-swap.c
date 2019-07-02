05ae7dfe5be4b46b60c9efda07415e104658ec86
humeinstein <humeincreations@gmail.com>
2019-07-02 13:31:19 +0000

    add proper comments

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,17 @@
 #include "holberton.h"
 #include <stdlib.h>
+
 /**
- *
- *
- *
+ * swap_int - function with int *a, int *b
+ * @b: variable used
+ * @a: variable used
  *
  */
 
 void swap_int(int *a, int *b)
 {
 	int box;
+
 	box = *a;
 	*a = *b;
 	*b = box;
