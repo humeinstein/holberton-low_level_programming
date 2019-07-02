ba87dfb08e9a72874e8ae861b622274475311b14
humeinstein <humeincreations@gmail.com>
2019-07-02 17:22:21 +0000

    add function that reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+/**
+ * rev_string - reverses string
+ * string_length - finds length of the string
+ * @s: s str
+ *
+ * string_length - (char *pointer)
+ */
+void rev_string(char *s)
+{
+	int string_length(char *);
+	void reverse(char *);
+	int length, c;
+	char *begin, *end, temp;
+
+	length = string_length(s);
+	begin = s;
+	end = s;
+
+	for (c = 0 ; c < length - 1 ; c++)
+	{
+		end++;
+	}
+	for (c = 0 ; c < length / 2 ; c++)
+	{
+		temp = *end;
+		*end = *begin;
+		*begin = temp;
+
+		begin++;
+		end--;
+	}
+}
+
+int string_length(char *pointer)
+{
+	int c = 0;
+
+	while (*(pointer + c) != '\0')
+	{
+		c++;
+	}
+	return (c);
+}
