b9534ffa990478cb3ba6998f2f17da2d86d8b961
humeinstein <humeincreations@gmail.com>
2019-07-02 13:33:41 +0000

    add comments to function

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,16 @@
 #include "holberton.h"
+
 /**
+ * _strlen - function
  *
- *
- *
- *
+ * @s: char s
+ * Return: (length)
  */
 
 int _strlen(char *s)
 {
 	int length = 0;
+
 	while (*s != '\0')
 	{
 		length++;
