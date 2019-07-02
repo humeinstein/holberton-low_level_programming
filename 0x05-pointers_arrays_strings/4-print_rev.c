50bd4454316adbd4c8cfa66ccb089ca4995934db
humeinstein <humeincreations@gmail.com>
2019-07-02 14:43:41 +0000

    print a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_rev - function s
+ * @s: pointer s
+ *
+ *
+ *
+ */
+void print_rev(char *s)
+{
+	int len;
+
+	for (len = 0 ; s[len] != '\0' ; len++)
+	{
+	}
+	for (len = len - 1 ; len >= 0 ; len--)
+	{
+		_putchar(s[len]);
+	}
+	_putchar('\n');
+}
