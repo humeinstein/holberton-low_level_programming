29db5984ab0dde5db2ba7126a0b99140b7ac43d4
humeinstein <humeincreations@gmail.com>
2019-07-03 02:06:09 +0000

    function that prints n elements of the array to be printed

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,19 +1,19 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- *
- *
- *
+ * print_array - int a int n
+ * @a: array
+ * @n: variable
  *
  */
-void print_array(int *a, int n )
+void print_array(int *a, int n)
 {
 	int i;
 
 	for (i = 0 ; i < n ; i++)
 	{
 		printf("%d", a[i]);
-		if (i != (n-1))
+		if (i != (n - 1))
 		{
 			printf(", ");
 		}
