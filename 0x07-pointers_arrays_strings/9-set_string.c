8042acc24c2185019661265c99feced7ab340ffa
vagrant <vagrant@precise32.(none)>
2019-07-10 00:26:14 +0000

    function that sets value of a pointer to char

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * set_string - char pointer to other pointer
+ * @s: double pointer
+ * @to: pointed to
+ *
+ *
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
