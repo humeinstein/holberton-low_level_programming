a703160201b8b467aff8484685606818005466c1
vagrant <vagrant@precise32.(none)>
2019-07-10 18:21:57 +0000

    function that finds strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _strlen_recursion - char s find legnth
+ * @s: string
+ *
+ * Return: 1 + _strlen_recursion(s + 1)
+ *
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return 0;
+	}
+	else
+	{
+		return 1 + _strlen_recursion(s + 1);
+	}
+}
