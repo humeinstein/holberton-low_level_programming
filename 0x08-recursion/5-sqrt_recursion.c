93aaa53ca920d535c6584009b28110db9ef73c0e
vagrant <vagrant@precise32.(none)>
2019-07-10 20:43:39 +0000

    function that returns the natural aquare root of a #

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,30 +1,35 @@
 #include "holberton.h"
 /**
- *
- *
- *
- *
+ * _teeth - add variable
+ * @n: orig
+ * @x: incre
+ * Return: -1 , x , n, 1
  */
 int _teeth(int n, int x)
 {
-        if (x * x < n)
-        {
-                return _teeth(n, x + 1);
-        }
-        else if (x * x == n)
-        {
-                return(x);
-        }
-        else
-        {
-                return (-1);
-        }
+	if (x * x < n)
+	{
+		return (_teeth(n, x + 1));
+	}
+	else if (x * x == n)
+	{
+		return (x);
+	}
+	else
+	{
+		return (-1);
+	}
 }
+/**
+ * _sqrt_recursion - get sqrt
+ * @n: int
+ * Return: _teeth n,1 , -1
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 	{
 		return (-1);
 	}
-	return _teeth(n, 1);
+	return (_teeth(n, 1));
 }
