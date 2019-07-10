647c633dc40bae5c4be684a13583a241c7c86e99
vagrant <vagrant@precise32.(none)>
2019-07-10 17:12:22 +0000

    error fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,15 +8,11 @@
  */
 void _puts_recursion(char *s)
 {
-
-	_putchar(*s);
-	if (*s != '\0')
-	{
-		_puts_recursion(s + 1);
-		return;
-	}
-	else
+	if (*s == '\0')
 	{
 		_putchar('\n');
+		return;
 	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
