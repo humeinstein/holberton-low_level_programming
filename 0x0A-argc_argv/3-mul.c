3816ef09e174331c8d3c941975886f094749d1da
vagrant <vagrant@precise32.(none)>
2019-07-14 20:46:24 +0000

    multiplcation of two arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiple 2 arguments
+ * @argc: args
+ * @argv: storage
+ *
+ * Return: 0 if 2 to multiply and 1 for else
+ */
+int main(int argc, char *argv[])
+{
+	int i, x;
+
+	if (argc == 3)
+	{
+		i = atoi(argv[1]);
+		x = atoi(argv[2]);
+		i = i * x;
+		printf("%d\n", i);
+		return (0);
+	}
+	else
+	{
+		printf("Error");
+		return (1);
+	}
+}
