83a10d18a63f06eff687b79e4faabc3a90a5947f
vagrant <vagrant@precise32.(none)>
2019-07-14 18:27:23 +0000

    it aint what they call you, its what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - program to print its name
+ * @argc: show function name
+ * @argv: argument count
+ */
+
+int main(int argc, char **argv[])
+{
+	printf("%s \n", argv[0]);
+	(void)argc;
+	return (0);
+}
