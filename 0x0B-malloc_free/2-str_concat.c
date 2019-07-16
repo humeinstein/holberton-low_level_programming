77f5f0fb2866463df436a38f573fd3c31abfcfe1
vagrant <vagrant@precise32.(none)>
2019-07-16 21:59:10 +0000

    error fix betty

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,10 +15,12 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
+		s1 = malloc(sizeof(char));
 		s1 = '\0';
 	}
 	if (s2 == NULL)
 	{
+		s2 = malloc(sizeof(char));
 		s2 = '\0';
 	}
 	for (count = 0 ; s1[count] != '\0' ; count++)
