bb9a315c2dffca03f398a4156322afe34305fdd4
vagrant <vagrant@precise32.(none)>
2019-07-16 17:05:25 +0000

    test

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,7 +5,7 @@
  * create_array - creates an array
  * @size: how many
  * @c: char
- * Return: NULL or d 
+ * Return: NULL or d
  *
  *
  */
@@ -13,11 +13,12 @@ char *create_array(unsigned int size, char c)
 {
 	char *d;
 	unsigned int i;
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
-        d = malloc(sizeof(*d) * size);
+	d = malloc(sizeof(*d) * size);
 	for (i = 0 ; i < size ; i++)
 	{
 		d[i] = c;
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+<h1>Malloc && FREE</h1>
