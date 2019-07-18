c059de8714e7f8e04d6df69d1e0473fbb3670a51
vagrant <vagrant@precise32.(none)>
2019-07-18 17:13:35 +0000

    test

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+int **alloc_grid(int width, int height)
+{
+	int **g, k, p;
+
+	if (width == 0 || height == 0)
+	{
+		return (NULL);
+	}
+	g = malloc(sizeof(int *) * height);
+
+	if (g == NULL)
+	{
+		free(g);
+		return (NULL);
+	}
+	for (k = 0 ; k < height ; k++)
+	{
+		g[k] = (int *)malloc(width * sizeof(int));
+		if (g[k] == NULL)
+		{
+			for (; g >= 0;)
+			{
+				free(g[k]);
+				k--;
+			}
+			free(g);
+			return (NULL);
+		}
+	}
+	for (k = 0 ; k < height ; k++)
+	{
+		for (p = 0 ; p < width ; p++)
+		{
+			g[k][p] = 0;
+		}
+	}
+	return (g);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -4,4 +4,5 @@ char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
 int _putchar(char c);
 char *str_concat(char *s1, char *s2);
+char *argstostr(int ac, char **av);
 #endif
