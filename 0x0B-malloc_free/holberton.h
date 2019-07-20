abbd577287048f920ad90de5efb0c63712cd5696
vagrant <vagrant@precise32.(none)>
2019-07-20 17:30:37 +0000

    test

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -5,4 +5,5 @@ char *create_array(unsigned int size, char c);
 int _putchar(char c);
 char *str_concat(char *s1, char *s2);
 char *argstostr(int ac, char **av);
+int **alloc_grid(int width, int height);
 #endif
