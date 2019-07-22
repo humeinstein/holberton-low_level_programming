9937bb1c7c2461cb1213936cbd52cc0e26ab2ff7
vagrant <vagrant@precise32.(none)>
2019-07-22 21:18:10 +0000

    hello test

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -40,14 +40,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dg;
 
-	if (name == NULL || owner == NULL)
-	{
-		return (NULL);
-	}
 	dg = malloc(sizeof(dog_t));
 	if (dg == NULL)
 	{
-		return (dg);
+		free(dg);
 		return (NULL);
 	}
 	dg->name = _strdup(name);
