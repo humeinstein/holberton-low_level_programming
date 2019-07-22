979bd43561c75e9c2cf3bb920ae15c3de0e145b1
vagrant <vagrant@precise32.(none)>
2019-07-22 17:02:19 +0000

    readme && dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifndef DOG_H_
+#define DOG_H_
+/**
+ * struct dog - name,age,owner
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
