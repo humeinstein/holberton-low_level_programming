4c789cb4689fe4b745954e1bcde954b545b1726a
vagrant <vagrant@precise32.(none)>
2019-07-27 00:50:14 +0000

    function to print strings of variety

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdargs.h>
+/**
+ * print_strings - print strings
+ * @separator: separator char
+ * @n: iterator thru the string index
+ *
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list vl;
+	unsigned int x;
+	char *b;
+
+	va_start(vl, n);
+	for (x = 0; x < n; x++)
+	{
+		b = va_arg(vl, char*);
+		if (b != NULL)
+			printf("%s", b);
+		else
+			printf("%p", b);
+		if (separator != NULL && x < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(vl);
+}
