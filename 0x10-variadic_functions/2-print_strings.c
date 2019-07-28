d94097e1b7e23e91bb812b4f26c5e9096ebfa4e3
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2019-07-28 01:04:36 -0400

    Update 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("%p", b);
 		}
-		else (separator != NULL && x < n - 1)
 			printf("%s%s", separator, s);
 	}
 	printf("\n");
