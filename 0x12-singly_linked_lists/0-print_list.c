3d0e8cf138c86187710640acc829e673004c0da8
humeinstein <humeincreations@gmail.com>
2019-08-01 23:50:59 +0000

    typo

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,7 +9,7 @@
 
 size_t print_list(const list_t *h)
 {
-	int x;
+	int x = 0;
 
 	while (h != NULL)
 	{
@@ -18,9 +18,9 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		}
 		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
+			printf("[%i] %s\n", h->len, h->str);
 		x++;
+		h = h->next;
 	}
 	return(x);
 }
