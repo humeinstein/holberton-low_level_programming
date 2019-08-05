4f672294e633f7e7d5197a1936544975d0e7fc00
humeinstein <humeincreations@gmail.com>
2019-08-05 18:09:34 +0000

    changed var declaration from int to unsigned

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,7 +9,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t  *new;
-	int x = 0;
+	unsigned int x = 0;
 
 	new = malloc(sizeof(list_t));
 
@@ -19,13 +19,12 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 	new->str = strdup(str);
-	while (new->str)
+	while (str[x])
 	{
 		x++;
 	}
 	new->len = x;
 	new->next = *head;
 	*head = new;
-	free (new);
 	return (*head);
 }
