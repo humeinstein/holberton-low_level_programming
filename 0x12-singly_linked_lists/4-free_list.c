d3a656dd180f37d2b9ae59db903874dc93861f03
humeinstein <humeincreations@gmail.com>
2019-08-05 19:12:19 +0000

    update lists.h, function to free singly linked lists

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,13 +1,20 @@
 #include "list.h"
 /**
- *
- *
+ * free_list - function that frees a linked list
+ * @head: element to be freed
  *
  *
  *
  */
 void free_list(list_t *head)
 {
-	free (list_t head);
+	list_t *new;
 
+	while (head != '\0')
+	{
+		new = head;
+		free(new->str);
+		free(new);
+		head = head->next;
+	}
 }
