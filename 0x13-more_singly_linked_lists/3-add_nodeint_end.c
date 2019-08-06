6fb849282f222a2b2d6dfda7fa36f918e925425c
humeinstein <humeincreations@gmail.com>
2019-08-06 14:24:10 +0000

    final rev

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -27,7 +27,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	else
 	{
 		newer = *head;
-		while (new->next != NULL)
+		while (newew->next != NULL)
 		{
 			newer = newer->next;
 		}
