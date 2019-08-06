eedbf252ef06b997ca8e17e83a4ead81abc91bf7
humeinstein <humeincreations@gmail.com>
2019-08-06 17:09:17 +0000

    function to get sum of all elements in a list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * sum_listint - get sum of all ele
+ * @head: ptr to node
+ * Return: sum
+ *
+ */
+int sum_listint(listint_t *head)
+{
+	int x;
+
+	for (x = 0; head != '\o'; head = head->next)
+		x = x + head->n;
+
+	return (x);
+}
