c5fa1918031b60c11b8d58136bde4585dda55011
humeinstein <humeincreations@gmail.com>
2019-08-06 16:18:43 +0000

    function that deletes head node but prints its data

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * pop_listint - delete head node
+ * @head: pointer to ele
+ *
+ * Return: head data after free or 0 if empty
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t new;
+	int x;
+
+	while (*head != NULL)
+	{
+		new = *head;
+		x = new->x;
+		new = new->next;
+		free(*head);
+		*head = new;
+	}
+	if (*head == NULL)
+	{
+		return (0);
+	}
+	else
+		return (x);
+}
