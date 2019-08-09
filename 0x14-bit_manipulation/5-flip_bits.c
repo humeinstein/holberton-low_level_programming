1c33a7d8ad97c7aa9720d9be36bdee2d78857046
humeinstein <humeincreations@gmail.com>
2019-08-09 02:28:06 +0000

    function that gets # of flips

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * flip_bits - function that returns # of bits flipped form 1 to another
+ * @n: int
+ * @m: int
+ * @Return: # of flips
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int differences = n ^ m;
+	unsigned long int x, y, numb;
+
+	for (x = 0; differences != 0; x++)
+	{
+		y = differences & 1;
+		differences >>= 1;
+		if (y == 1)
+		{
+			numb++;
+		}
+	}
+	return (numb);
+}
