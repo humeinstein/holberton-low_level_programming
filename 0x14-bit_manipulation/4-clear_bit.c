95ccf0f9e0e528c901856e7c4b7c5afa51739271
humeinstein <humeincreations@gmail.com>
2019-08-09 02:12:15 +0000

    fixed bitwise and betty

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,14 +1,16 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * clear_bit - function that clears bit at index
+ * @n: int
+ * @index: index of where to clear bit
+ * Return: -1 if fail
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index < 64)
 	{
 		*n &= ~(1 << index);
+		return (1);
 	}
 	return (-1);
 }
