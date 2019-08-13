cce7053f495d6f02b9187da5745bd67ab0c73cef
humeinstein <humeincreations@gmail.com>
2019-08-13 19:33:57 +0000

    new push

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,26 +12,24 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fileop;
 	char *space;
 
-	space = malloc(sizeof(char) * letters);
-	if (space == NULL)
-	{
-		free(space);
-		return (0);
-	}
-	filepp = open(filename, O_RDONLY);
-	fileop = read(filepp, space, letters);
-
 	if (filename == NULL)
 		return (0);
 	if (letters == NULL)
+		return (0);
+
+	space = malloc(sizeof(char) * letters);
+	if (space == NULL)
 	{
+		close(filepp);
 		return (0);
 	}
+	filepp = open(filename, O_RDONLY);
+	if (filepp == -1)
+		return (0);
+	fileop = read(filepp, space, letters);
+
 	write(STDOUT_FILENO, space, fileop);
-	if (fileop < 1)
-	{
-		return (0);
-	}
+
 	free(space);
 	close(filepp);
 	return (fileop);
