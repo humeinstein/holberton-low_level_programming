4e2ddc73a065964599b4e857801e1740ecb801b3
humeinstein <humeincreations@gmail.com>
2019-09-14 16:57:22 +0000

    [STYLE]betty

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,9 +1,8 @@
 #include "lists.h"
 /**
- *
- *
- *
- *
+ * dlistint_len - function that prints # of ele in a list
+ * @h: pointer to curr ele
+ * Return: count
  */
 size_t dlistint_len(const dlistint_t *h)
 {
