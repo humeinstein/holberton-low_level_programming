be2e15ec18049dcd29f075a79918ec4351a9deba
humeinstein <humeincreations@gmail.com>
2019-09-14 16:03:27 +0000

    [INIT]function that prints all elements od DLI

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,20 +1,19 @@
 #include "lists.h"
 /**
- *
- *
- *
+ * print_dlistint - function that prints all elements of dli
+ * @h: pointer to current node
+ * Return: inc how many elements
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t x;
-	x = 0;
+	size_t inc;
+	inc = 0;
 
 	while (h != NULL)
 	{
-		x++;
-		printf("%d\n", h->next);
+		inc++;
+		printf("%d\n", h->n);
 		h = h->next;
-		x++;
 	}
-	return (x);
+	return (inc);
 }
