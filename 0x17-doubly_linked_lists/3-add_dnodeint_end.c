6ed25d199a69773385dd12b6acc65b032448560c
humeinstein <humeincreations@gmail.com>
2019-09-15 20:53:47 +0000

    [INIT]function that adds node at end of doubly linked list

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ *
+ *
+ *
+ *
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+	dlistint_t *last = *head;
+
+	new = malloc(sizeof(dlistint_t));
+
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new;
+	new->prev = last;
+
+	return (*head);
+}
