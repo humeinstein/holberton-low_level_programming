f8a8f5f9d1dbe04bf68dd798b14f6c993c781ec6
humeinstein <humeincreations@gmail.com>
2019-09-15 20:58:38 +0000

    [STYLE]betty fix

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 /**
- *
- *
- *
- *
+ * add_dnodeint_end - function that adds node to end of dbl
+ * @head: node
+ * @n: value
+ * Return: *head
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
@@ -11,7 +11,11 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *last = *head;
 
 	new = malloc(sizeof(dlistint_t));
-
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 	new->n = n;
 	new->next = NULL;
 
