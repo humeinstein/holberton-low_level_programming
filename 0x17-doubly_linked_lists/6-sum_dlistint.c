8ec21ecc96c5f577b5d7d327ad8679f00555f54d
humeinstein <humeincreations@gmail.com>
2019-09-16 01:02:22 +0000

    [INIT]function to add value of all nodes, prototype in lists.h

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * sum_dlistint - function that adds value of all ndoes
+ * @head: ptr 2 list
+ * Return: total
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *new = head;
+	int total = 0;
+
+	if (new == NULL)
+	{
+		return (total);
+	}
+	while (new != NULL)
+	{
+		total = total + search->n;
+		new = new->next;
+	}
+	return (total);
+}
