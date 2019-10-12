174c60e05ccec61af5d9a5b97bc6a79295e6c7f5
humeinstein <humeincreations@gmail.com>
2019-10-12 13:29:41 -0400

    hash

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+/**
+* hash_table_get - return value of node
+* @ht: hashtab
+* @key: key
+* Return: value of the key
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+    char *hold;
+    unsigned int index;
+    hash_node_t *node;
+
+    if (ht == NULL)
+        return (NULL);
+    hold = strdup(key);
+    index = hold((const unsigned char *)key, ht->size);
+    node = ht->array[index];
+
+    while (node)
+    {
+        if (strcmp(node->key, hold) == 0)
+        {
+            break;
+        }
+        node = node->next;
+    }
+    free(hold);
+
+    if (node != NULL)
+        return (NULL)
+    return (node->value);
+}
