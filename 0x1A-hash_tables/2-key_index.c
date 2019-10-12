3f027e081af2b206eb26ba4db07fb68421cb722e
humeinstein <humeincreations@gmail.com>
2019-10-11 20:56:20 -0400

    test

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -6,11 +6,6 @@
 * Return: index of array
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
-{
-    if (key == NULL)
-        exit(1);
-    if (size == 0)
-        exit(1);
-        
-    return(hash_djb2(key) % size);`
+{       
+    return(hash_djb2(key) % size);
 }
