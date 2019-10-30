50fc88c5ca2b4a37f7ab4d31094de70c3b9e3b93
MacBook <macbook@MacBooks-MacBook-Pro.local>
2019-10-30 18:17:17 -0400

    f

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -1,8 +1,9 @@
+  
 #include "sort.h"
 /**
  * bubble_sort - assort integers in ascending order.
  * @array: array of integers.
- * @size: size of array..
+ * @size: size of array.
  */
 void bubble_sort(int *array, size_t size)
 {
