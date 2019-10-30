7a026120e40cc2fb04e08de96194f1861f36c61e
MacBook <macbook@MacBooks-MacBook-Pro.local>
2019-10-30 19:07:57 -0400

    d

---
+++ 0x1B-sorting_algorithms/1000-sort_deck.c
@@ -0,0 +1,10 @@
+#include "deck.h"
+/**
+ * sort_deck - sort a deck of cards.
+ * @deck: deck of cards.
+ */
+void sort_deck(deck_node_t **deck)
+{
+	if (deck == NULL || *deck == NULL)
+		return;
+}
