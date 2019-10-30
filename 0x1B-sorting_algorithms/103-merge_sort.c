6802d39133df91e105e6a31d277f9be91590bd58
MacBook <macbook@MacBooks-MacBook-Pro.local>
2019-10-30 19:04:16 -0400

    fh

---
+++ 0x1B-sorting_algorithms/103-merge_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+/**
+ * merge_sort - merge sort algorithm.
+ * @array: array of integers.
+ * @size: size of array.
+ */
+void merge_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+}
