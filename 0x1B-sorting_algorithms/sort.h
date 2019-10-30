64452b19086d56b5cd0fbab449dd4b041af2eafb
MacBook <macbook@MacBooks-MacBook-Pro.local>
2019-10-30 18:25:32 -0400

    FINALLY????

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -1,8 +1,8 @@
-#ifndef SORT_H_
-#define SORT_H_
+#ifndef _SORT_H_
+#define _SORT_H_
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
 /**
@@ -14,17 +14,25 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
-
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
 void bubble_sort(int *array, size_t size);
-void swap(int *xp, int *yp);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
 #endif
