26fa6a54335871763d3cbca1ee9df3bd1b940161
MacBook <macbook@MacBooks-MacBook-Pro.local>
2019-12-06 21:11:40 -0500

    INIT: function checks if binary tree is full

--- 0x1D-binary_trees/15-binary_tree_is_full.c
+++ 0x1D-binary_trees/15-binary_tree_is_full.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 /**
-*
-*
-*
+* binary_tree_is_full - checks if btree is full
+* @tree: pointer to root node
+* Return: 0 if not full 1 if full
 *
 */
 int binary_tree_is_full(const binary_tree_t *tree)
