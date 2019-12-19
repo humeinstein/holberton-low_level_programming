20a2ccf8ab9b5e5c084966c6c9c818f87aee8574
humeinste <humeincreations@gmail.com>
2019-12-19 19:13:46 +0000

    FIX: formatting

--- 0x1D-binary_trees/17-binary_tree_sibling.c
+++ 0x1D-binary_trees/17-binary_tree_sibling.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+
 /**
 * binary_tree_sibling - finds the sibling
 * @node: node to find sibling of
@@ -6,13 +7,10 @@
 */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (node == NULL)
+    if (node == NULL || node->parent == NULL)
         return (NULL);
-    if (node->parent == NULL)
-        return (NULL;
-        
+            
     if (node->parent->right == node)
         return (node->parent->left);
-    else
-        return (node->parent->right);
+    return (node->parent->right);
 } 
